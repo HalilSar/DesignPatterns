d3b98884316a7d85dcf9c089b4cc90b59828bb2e
halil37 <hllibrahimsartitas@gmail.com>
2024-02-09 19:29:33 +0300

    bmwfactory created.

---
+++ src/DesignPatterns.Creational.Factory/BMWFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public class BMWFactory : CarFactory
+    {
+        public override void CreateAuto()
+        {
+            SetCarList(new Z4(170));
+            
+        }
+    }
+}
