cfc3bbccc9f36c919c7341df054bae00e17f4071
halil37 <hllibrahimsartitas@gmail.com>
2024-02-09 19:06:23 +0300

    z4 classı oluşturuldu.

---
+++ src/DesignPatterns.Creational.Factory/Z4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public class Z4:Car
+    {
+        public Z4(int beygirGücü):base("BMW", "Z4", beygirGücü)
+        {
+          
+        }
+    }
+}
