9165025c267e39489bec08df1afbef6f987f5173
halil37 <hllibrahimsartitas@gmail.com>
2024-02-09 19:31:43 +0300

    BMW3 created.

---
+++ src/DesignPatterns.Creational.Factory/BMW3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public class BMW3 : Car
+    {
+        public BMW3(int beygirGücü):base("BMW", "BMW3", beygirGücü)
+        {
+          
+        }
+    }
+}
