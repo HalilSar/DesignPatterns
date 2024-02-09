eb54dd68222162e3998b8df33b82f8f04affc077
halil37 <hllibrahimsartitas@gmail.com>
2024-02-09 19:01:59 +0300

    CarFactory created.

---
+++ src/DesignPatterns.Creational.Factory/CarFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+   public abstract class CarFactory
+    {
+        List<Car> cars = new List<Car>();
+        public abstract void CreateAuto();
+        public CarFactory()
+        {
+            CreateAuto();
+        }
+        public void SetCarList(Car car) => this.cars.Add(car);
+
+
+    }
+}
