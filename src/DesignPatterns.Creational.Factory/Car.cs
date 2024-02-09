f99fb2f713dd3f555f78ac78f33b570d1b836bbe
halil37 <hllibrahimsartitas@gmail.com>
2024-02-09 18:51:30 +0300

    car abstrac class created.

---
+++ src/DesignPatterns.Creational.Factory/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+   public abstract class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; } // 100-200
+
+        public Car(string brand,string model,int horsePower)
+        {
+            Brand = brand;
+            Model = model;
+            HorsePower = horsePower;
+        }
+    }
+}
