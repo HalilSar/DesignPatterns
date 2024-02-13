e5b406e0c763c54cfca01a29bb418f35c7b31993
halil37 <hllibrahimsartitas@gmail.com>
2024-02-13 20:11:05 +0300

    modified in project.

--- src/DesignPatterns.Creational.AbstractFactory/Program.cs
+++ src/DesignPatterns.Creational.AbstractFactory/Program.cs
@@ -6,7 +6,17 @@ namespace DesignPatterns.Creational.AbstractFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbstractFactory factory = new ConcreteFactory1();
+            var bear = factory.GetBear();
+            var tiger = factory.GetTiger();
+            Console.WriteLine("Bear:" + " " + bear.GetBearSpecies());
+            Console.WriteLine("Tiger:" + " " + tiger.GetTigerSpecies());
+            Console.WriteLine("**************************");
+            factory = new ConcreteFactory2();
+            bear = factory.GetBear();
+            tiger = factory.GetTiger();
+            Console.WriteLine("Bear:" + " " + bear.GetBearSpecies());
+            Console.WriteLine("Tiger:" + " " + tiger.GetTigerSpecies());
         }
     }
 }
