f3e1843ae23edf8c5416d83e2a23650a48918328
halil37 <hllibrahimsartitas@gmail.com>
2024-02-13 19:55:31 +0300

    concretefactory1 added to project.

---
+++ src/DesignPatterns.Creational.AbstractFactory/ConcreteFactory1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.AbstractFactory
+{
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public Bear GetBear()
+        {
+            return   new PolarBear();
+
+        }
+
+        public Tiger GetTiger()
+        {
+            return new BengalTiger();
+
+          
+        }
+    }
+}
