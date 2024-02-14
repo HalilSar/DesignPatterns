dd0947eee6f3e43e596b0974815daeaa469603eb
halil37 <hllibrahimsartitas@gmail.com>
2024-02-14 21:44:58 +0300

    Brand added in project.

---
+++ src/DesignPatterns.Creational.Builder/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Builder
+{
+   public  class Brand
+    {
+        private string BrandName { get; set; }
+        public Brand(string brandName)
+        {
+            BrandName = brandName;
+        }
+        public override string ToString()
+        {
+            return BrandName;
+        }
+    }
+}
