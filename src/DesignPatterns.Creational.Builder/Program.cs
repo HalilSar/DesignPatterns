9ad29435b1597e6daa3c8f5c59cb9857339f7dca
halil37 <hllibrahimsartitas@gmail.com>
2024-02-14 21:30:00 +0300

    Builder project added in src

---
+++ src/DesignPatterns.Creational.Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
