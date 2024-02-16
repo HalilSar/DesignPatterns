7fd41baad1c5e014d94fdf113a11aa229687191b
halil37 <hllibrahimsartitas@gmail.com>
2024-02-16 20:48:25 +0300

    personal account created.

---
+++ src/DesignPatterns.Creational.Protype/PersonalAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Prototype
+{
+    public abstract class PersonalAccount : AccountPrototype
+    {
+        public string Name;
+        public string SurName { get; set; }
+        public PersonalAccount(decimal balance)
+        {
+            this.Balance = balance;
+        }
+    }
+}
