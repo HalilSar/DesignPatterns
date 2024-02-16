74a4b31232eda77348b86c4a0e0a4f842f80eaf2
halil37 <hllibrahimsartitas@gmail.com>
2024-02-16 20:05:14 +0300

    Protype designpattern added in project.

---
+++ src/DesignPatterns.Creational.Protype/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Protype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    //class DummyProtype : ICloneable
+    //{
+
+    //}
+}
