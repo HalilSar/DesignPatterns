b9729aa711944a11132a7bb1ea2124bbb45d3c91
halil37 <hllibrahimsartitas@gmail.com>
2024-02-20 21:55:23 +0300

    printscreen added in project.

---
+++ src/DesignPatterns.Structural.ObjectAdapter/PrintScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.ObjectAdapter
+{
+    public class PrintScreen
+    {
+        public void Screen()
+        {
+            Console.WriteLine("Screen method run at PrintScreen object.");
+        }
+    }
+}
+}
