578c890c101256a41b9e37bb4969044cecbad49d
halil37 <hllibrahimsartitas@gmail.com>
2024-02-21 20:17:04 +0300

    printa created and modified.

---
+++ src/DesignPatterns.Structural.Bridge/PrintA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Bridge
+{
+    public  class PrintA
+    {
+        public void PrintFootballMagazine() => Console.WriteLine("PrintA: Football Magazine Printed");
+        public void PrintFantasticBook() => Console.WriteLine("PrintA: Fantastic Book Printed");
+    }
+}
