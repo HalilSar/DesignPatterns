729980cee5f77fa67892cec752ffb4b238ad318b
halil37 <hllibrahimsartitas@gmail.com>
2024-02-21 20:34:43 +0300

    Magazine added to project.

---
+++ src/DesignPatterns.Structural.Bridge/Magazine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Bridge
+{
+    public class Magazine:Catalog
+    {
+        public Magazine(Publish publish):base(publish)
+        {
+            
+        }
+        public override  void CreatePublish()
+        {
+            CreateMagazinePublish();
+        }
+        public void CreateMagazinePublish()
+        {
+            this.publish.PublishMagazine();
+        }
+
+
+    }
+}
