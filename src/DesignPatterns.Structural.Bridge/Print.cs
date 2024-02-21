680ce385ae69ad05a24a5c6de8f8b9716eaf65cb
halil37 <hllibrahimsartitas@gmail.com>
2024-02-21 20:05:51 +0300

    Print interface added and modified to project.

---
+++ src/DesignPatterns.Structural.Bridge/Print.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Bridge
+{
+   public interface Print
+    {
+        public void PublishBook();
+        public void PublishMagazine();
+    }
+}
