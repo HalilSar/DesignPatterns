528d2af690011bc025f4e52eec7771d76b36fa0d
halil37 <hllibrahimsartitas@gmail.com>
2024-02-23 21:07:56 +0300

    Computer added in project.

---
+++ src/DesignPatterns.Structural.Composite/Computer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Composite
+{
+    public class Computer : Part
+    {
+        private  List<Part> partList = new List<Part>();
+        public void AddPart(Part part) => partList.Add(part);
+        public void RemovePart(Part part) => partList.Remove(part);
+        public Part GetPart(int id) => partList[id];
+        
+        public int GetPrice()
+        {
+            int price = 0;
+            for (int i = 0; i < partList.Count; i++)
+            {
+                price += GetPart(i).GetPrice();
+            }
+            return price;
+        }
+    }
+}
