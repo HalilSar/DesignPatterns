2fbfd72db43914b5829842749d1b1813b52f7880
halil37 <hllibrahimsartitas@gmail.com>
2024-02-25 00:47:55 +0300

    coffecup added.

---
+++ src/DesignPatterns.Structural.Decorator/CoffeeCup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Decorator
+{
+    public class CoffeeCup : CupDecorator
+    {
+        public override void Produce()
+        {
+            GetCup().Produce();
+            AddGrip();
+        }
+
+        public void AddGrip() => Console.WriteLine("Handle add to cup");
+    }
+}
