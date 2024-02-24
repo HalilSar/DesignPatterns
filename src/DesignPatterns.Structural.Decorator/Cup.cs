feaa61b5129bea98c328d7f77e9f5ede7d5b11cf
halil37 <hllibrahimsartitas@gmail.com>
2024-02-25 00:34:00 +0300

    cup added.

---
+++ src/DesignPatterns.Structural.Decorator/Cup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Decorator
+{
+    public class Cup : KitchenStuff
+    {
+        public void Produce()
+        {
+            Console.WriteLine("Cup produce.");
+        }
+    }
+}
