91fef5b97a631ca3cf4d6053fd2191353678955a
halil37 <hllibrahimsartitas@gmail.com>
2024-02-26 20:12:59 +0300

    Character added in project.

---
+++ src/DesignPatterns.Structural.Flyweight/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Flyweight
+{
+    public class Character
+    {
+        private string characterValue;
+        public Character(string character)
+        {
+            this.characterValue = character;
+
+        }
+
+        public string CharacterValue { 
+            get {
+                return this.characterValue;
+            } set { value= characterValue; } }
+        
+    }
+}
