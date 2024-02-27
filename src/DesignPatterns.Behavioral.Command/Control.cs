f67c61e369ca982ba372e44a15b2bc913c5b5964
halil37 <hllibrahimsartitas@gmail.com>
2024-02-27 22:32:31 +0300

    control added in command project

---
+++ src/DesignPatterns.Behavioral.Command/Control.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command
+{
+   public class Control
+    {
+        public Command[] commands = new Command[2];
+        public Control()
+        {
+            Television tv = new Television();
+            commands[0] = new TvOpenCommand(tv);
+            commands[1] = new TvCloseCommand(tv);
+        }
+
+        public void Press(int command)
+        {
+            if ( command>commands.Length  ||  command < 0)
+            {
+                Console.WriteLine("Invalid command");
+            }
+
+            commands[command].Execute();
+        }
+    }
+}
