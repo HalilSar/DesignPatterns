4b0eebe4112dcc47f024276dde7135ef169d96c0
halil37 <hllibrahimsartitas@gmail.com>
2024-02-27 22:14:06 +0300

    television added in command project.

---
+++ src/DesignPatterns.Behavioral.Command/Television.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command
+{
+   public class Television
+    {
+        public void Open() => Console.WriteLine("Tv opened.");
+
+        public void Close() => Console.WriteLine("Tv opened.");
+
+        public void Change() => Console.WriteLine("Channel Changed.");
+    }
+}
