6e9700911484fa9f6d09b2763f186bb99762079c
halil37 <hllibrahimsartitas@gmail.com>
2024-02-27 22:22:21 +0300

    tvclosecommand created in project.

---
+++ src/DesignPatterns.Behavioral.Command/TvCloseCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command
+{
+    public class TvCloseCommand : Command
+    {
+        private Television tv;
+        public TvCloseCommand( Television tv)
+        {
+            this.tv = tv;
+        }
+        public void Execute()
+        {
+            tv.Close();
+        }
+    }
+}
