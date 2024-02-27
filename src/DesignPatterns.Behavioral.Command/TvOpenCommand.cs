021e4e39f745d9cd7a2e35d920692da42006b891
halil37 <hllibrahimsartitas@gmail.com>
2024-02-27 22:19:54 +0300

    tvopencommand created in command project.

---
+++ src/DesignPatterns.Behavioral.Command/TvOpenCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command
+{
+    public class TvOpenCommand : Command
+    {
+        private Television tv;
+        public TvOpenCommand( Television tv)
+        {
+            this.tv = tv;
+        }
+        public void Execute()
+        {
+            tv.Open();
+        }
+    }
+}
