8ff77ef54fb013c8a2bdbd07438fbb5b39f95f6b
halil37 <hllibrahimsartitas@gmail.com>
2024-02-29 15:55:48 +0300

    iterator project created.

---
+++ src/DesignPatterns.Behavioral.Iterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
