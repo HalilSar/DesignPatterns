54a817cd42ccdb61f972d87f9c69cbaee2bad0ce
halil37 <hllibrahimsartitas@gmail.com>
2024-02-29 17:10:57 +0300

    memonto project modified.

---
+++ src/DesignPatterns.Behavioral.Memonto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Memonto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
