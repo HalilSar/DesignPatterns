8e1690a150224cc5ecc426af1adde458d99afbd7
halil37 <hllibrahimsartitas@gmail.com>
2024-03-01 22:54:04 +0300

    newspaper added in project.

---
+++ src/DesignPatterns.Behavioral.Observer/NewsPaper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Observer
+{
+    public interface NewsPaper
+    {
+        void AddSubscriber(Subscriber subscriber);
+        void DeleteSubscriber(Subscriber subscriber);
+    }
+}
