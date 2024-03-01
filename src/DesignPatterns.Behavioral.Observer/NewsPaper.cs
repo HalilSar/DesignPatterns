9b3dc53f54d3cb04c00b4c77ec820dbd68f5ebb2
halil37 <hllibrahimsartitas@gmail.com>
2024-03-01 22:59:15 +0300

    sun created in observer project.

--- src/DesignPatterns.Behavioral.Observer/NewsPaper.cs
+++ src/DesignPatterns.Behavioral.Observer/NewsPaper.cs
@@ -1,10 +1,11 @@
-﻿using System;
-
+﻿using System.Collections.Generic;
 namespace DesignPatterns.Behavioral.Observer
 {
     public interface NewsPaper
     {
         void AddSubscriber(Subscriber subscriber);
         void DeleteSubscriber(Subscriber subscriber);
+
+        List<Subscriber> GetNewsPaper();
     }
 }
