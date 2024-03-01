79862f73f2eea913ea2f394b211e1e9d5db05ace
halil37 <hllibrahimsartitas@gmail.com>
2024-03-01 23:04:53 +0300

    guardian added in observer project.

---
+++ src/DesignPatterns.Behavioral.Observer/Guardian.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Behavioral.Observer
+{
+    public class Gurdian : NewsPaper
+    {
+        private List<Subscriber> subcscribers = new List<Subscriber>();
+        public void AddSubscriber(Subscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void DeleteSubscriber(Subscriber subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+
+        public List<Subscriber> GetNewsPaper()
+        {
+            return this.subcscribers;
+        }
+    }
+}
