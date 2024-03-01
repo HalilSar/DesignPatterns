a16ceef57e5f4db35e2eb4178809f19442e370ea
halil37 <hllibrahimsartitas@gmail.com>
2024-03-01 22:32:46 +0300

    observer project added in solution.

---
+++ src/DesignPatterns.Behavioral.Observer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
