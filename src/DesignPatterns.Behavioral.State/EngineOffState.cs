c33d3b39c63b9d8592ace2175e701e504ec1b4fe
halil37 <hllibrahimsartitas@gmail.com>
2024-03-01 22:14:13 +0300

    EngineOffState added in state project.

---
+++ src/DesignPatterns.Behavioral.State/EngineOffState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.State
+{
+    public class EngineOffState : State
+    {
+        public void Off(Engine engine)
+        {
+            Console.WriteLine("Engine off.");
+        }
+
+        public void On(Engine engine)
+        {
+            engine.On(new EngineOnState());
+            Console.WriteLine("Engine on");
+        }
+    }
+}
