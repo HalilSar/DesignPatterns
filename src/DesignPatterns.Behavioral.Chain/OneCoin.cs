148f15d91a8cb9507a7fb6597c870a5c719b4096
halil37 <hllibrahimsartitas@gmail.com>
2024-03-02 18:34:44 +0300

    OneCoin added in chain project.

---
+++ src/DesignPatterns.Behavioral.Chain/OneCoin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Chain
+{
+    public class OneCoin:Coin
+    {
+
+        public override string ToString()
+        {
+            return "One Coin";
+        }
+
+        public OneCoin()
+        {
+            value = 100;
+        }
+    }
+}
