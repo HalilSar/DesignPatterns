9b20e1e3b6327dfb7e0fa879ec470f8f858a48e1
halil37 <hllibrahimsartitas@gmail.com>
2024-03-02 18:00:04 +0300

    chain project add to solution.

---
+++ src/DesignPatterns.Behavioral.Chain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Chain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
