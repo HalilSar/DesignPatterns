4dc3bafb5e366e4272d860566c855cbefa574173
halil37 <hllibrahimsartitas@gmail.com>
2024-03-03 23:33:30 +0300

    SortManager added in strategy project.

---
+++ src/DesignPatterns.Behavioral.Strategy/SortManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Strategy
+{
+   public class SortManager
+    {
+        private ISortStrategy sortStrategy;
+        public SortManager(ISortStrategy sortStrategy)
+        {
+            this.sortStrategy = sortStrategy;
+        }
+
+        public void AddSortArray(int[] array) => this.sortStrategy.Sort(array);
+    }
+}
