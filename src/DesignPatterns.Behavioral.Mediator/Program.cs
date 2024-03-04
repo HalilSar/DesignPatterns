481beff8fd4a94e17c5cd682273cbcdfdba5f8ca
halil37 <hllibrahimsartitas@gmail.com>
2024-03-04 21:34:38 +0300

    mediator added in solution.

---
+++ src/DesignPatterns.Behavioral.Mediator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
