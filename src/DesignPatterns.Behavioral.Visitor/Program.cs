7d1c73efad8cc9177770152af582f2fb36e44f7a
halil37 <hllibrahimsartitas@gmail.com>
2024-03-05 11:48:18 +0300

    visitor project created.

---
+++ src/DesignPatterns.Behavioral.Visitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
