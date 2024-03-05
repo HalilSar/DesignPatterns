12af9dde0e2eb0208feaad333fea4516295c94d5
halil37 <hllibrahimsartitas@gmail.com>
2024-03-05 11:53:28 +0300

    visitor added in visitor project and modified.

---
+++ src/DesignPatterns.Behavioral.Visitor/Visitor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DesignPatterns.Behavioral.Visitor
+{
+    public interface Visitor
+    {
+        void Visit(HRPrinter printer);
+        void Visit(LexMarkPrinter printer);
+    }
+}
