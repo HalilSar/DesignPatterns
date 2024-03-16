f84cdd5a9e8b2730ec99668bf23b10a5b0f964b1
halil37 <hllibrahimsartitas@gmail.com>
2024-03-16 22:46:12 +0300

    IBankAccount created and modified at dynamicProxy project.

---
+++ src/DesignPatterns.Structural.DynamicProxy/IBankAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace DesignPatterns.Structural.DynamicProxy
+{
+    public interface IBankAccount
+    {
+        void WithDrawMoney(decimal money);
+        void DepositeMoney(decimal money);
+        void SetName(string name);
+        void GetName();
+        void GetSurName();
+        void SetSurName(string surName);
+        void GetAccountStatus();
+        void SetAccountStatus(decimal money);
+        bool Cancel();
+    }
+}
