3a4b9adbc1e48d87872c8b83afa95c54c65290c0
halil37 <hllibrahimsartitas@gmail.com>
2024-03-20 21:59:18 +0300

    Log cs modified in dynamic proxy project.

---
+++ src/DesignPatterns.Structural.DynamicProxy/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using static System.Console;
+namespace DesignPatterns.Structural.DynamicProxy
+{
+    public class Log<T> : DynamicObject
+    {
+        private readonly T subject;
+        private Dictionary<string, int> methodCallCount = new Dictionary<string, int>();
+        public Log(T subject) => this.subject = subject;
+
+        public static I As<I>() where I:class
+        {
+            if (!typeof(I).IsInterface)
+            {
+                throw new ArgumentException("I must be an interface type!");
+            }
+        }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            try
+            {
+                WriteLine($"Invoking {subject.GetType().Name}.{binder.Name} with arguments [{string.Join(",",args )}]");
+                if (methodCallCount.ContainsKey(binder.Name)) methodCallCount[binder.Name]++;
+                else methodCallCount.Add(binder.Name, 1);
+
+                result = subject.GetType().GetMethod(binder.Name).Invoke(subject, args);
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
