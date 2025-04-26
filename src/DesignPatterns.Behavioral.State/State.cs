2f61403de91af5f3d60fa6f32542b30d25174044
hisar37 <hllibrahimsaritas@gmail.com>
2025-04-26 21:30:00 +0300

    State.cs modified.

--- src/DesignPatterns.Behavioral.State/State.cs
+++ src/DesignPatterns.Behavioral.State/State.cs
@@ -4,7 +4,9 @@ namespace DesignPatterns.Behavioral.State
 {
     public interface State
     {
+       // Paramater Engine engine
         void On(Engine engine);
+        // Paramater Engine engine
         void Off(Engine engine);
     }
 }
