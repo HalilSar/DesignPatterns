1c3121c9dbe47de656e34e9ce395c5b065bb767f
hisar37 <hllibrahimsaritas@gmail.com>
2025-05-11 23:35:45 +0300

    faxvisitor.cs modified.

--- src/DesignPatterns.Behavioral.Visitor/FaxVisitor .cs
+++ src/DesignPatterns.Behavioral.Visitor/FaxVisitor .cs
@@ -4,11 +4,12 @@ namespace DesignPatterns.Behavioral.Visitor
 {
     public class FaxVisitor : Visitor
     {
+        // Parameter : HRPrinter printer
         public void Visit(HRPrinter printer)
         {
             Console.WriteLine(printer.ToString() + " is faxing...");
         }
-
+        // Parameter : LexMarkPrinter printer
         public void Visit(LexMarkPrinter printer)
         {
             Console.WriteLine(printer.ToString() + " cannot fax.(not implemented.)");
