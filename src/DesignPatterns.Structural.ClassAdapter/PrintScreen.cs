e0294ba490d81db76eac03712b056f1c14826ab1
hisar37 <hllibrahimsaritas@gmail.com>
2025-07-10 22:13:01 +0300

    PrintScreen.cs modified.

--- src/DesignPatterns.Structural.ClassAdapter/PrintScreen.cs
+++ src/DesignPatterns.Structural.ClassAdapter/PrintScreen.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace DesignPatterns.Structural.ClassAdapter
 {
     public  class PrintScreen
-    {
+    { 
+        // parameter : string val
         public void Screen(string val)
         {
             Console.WriteLine(val);
