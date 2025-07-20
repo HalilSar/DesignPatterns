d1255073da8fd86079e4566f874fb1825eb97aa3
hisar37 <hllibrahimsaritas@gmail.com>
2025-07-20 20:59:07 +0300

    IBankAccount.cs modified.

--- src/DesignPatterns.Structural.DynamicProxy/IBankAccount.cs
+++ src/DesignPatterns.Structural.DynamicProxy/IBankAccount.cs
@@ -2,14 +2,26 @@
 {
     public interface IBankAccount
     {
+        // return : decimal money
         void WithDrawMoney(decimal money);
+        
+        // return : decimal money
         void DepositeMoney(decimal money);
+        
+        // return : string name
         void SetName(string name);
+        
         void GetName();
         void GetSurName();
+
+        // return : string name
         void SetSurName(string surName);
+
         void GetAccountStatus();
+ 
+        // return : decimal money
         void SetAccountStatus(decimal money);
+ 
         void Cancel();
         bool CancelStatus();
     }
