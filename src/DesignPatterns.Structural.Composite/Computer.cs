811850b921bdd6db37a082d929e8c19fd4c33276
hisar37 <hllibrahimsaritas@gmail.com>
2025-08-07 23:29:57 +0300

    Computer.cs modified.

--- src/DesignPatterns.Structural.Composite/Computer.cs
+++ src/DesignPatterns.Structural.Composite/Computer.cs
@@ -9,10 +9,18 @@ namespace DesignPatterns.Structural.Composite
     public class Computer : Part
     {
         private  List<Part> partList = new List<Part>();
+
+        // Parameter : Part part
         public void AddPart(Part part) => partList.Add(part);
+
+        // Parameter : Part part
         public void RemovePart(Part part) => partList.Remove(part);
+
+        // Return    : Part
+        // Parameter : int id
         public Part GetPart(int id) => partList[id];
         
+        // Return : int 
         public int GetPrice()
         {
             int price = 0;
