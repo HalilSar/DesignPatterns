dba1e546664309a2efc08f6556b51a928e726f08
hisar37 <hllibrahimsaritas@gmail.com>
2025-10-05 22:07:35 +0300

    BaseAccountBank.cs modified.

--- src/DesignPatterns.Structural.ProtectionProxy/BaseAccountBank.cs
+++ src/DesignPatterns.Structural.ProtectionProxy/BaseAccountBank.cs
@@ -4,13 +4,22 @@ namespace DesignPatterns.Structural.ProtectionProxy
 {
     public  interface BaseAccountBank
     {
+        // Parameter: decimal amount
         public void WithDrawMoney(decimal amount);
+        
+        // Parameter: decimal amount
         public void DepositMoney(decimal amount);
         public void GetName();
+
+        // Parameter: string name
         public void SetName(string name);
         public void GetSurName();
+
+        // Parameter: string surName
         public void SetSurName(string surName);
         public void GetAccountStatus();
+
+        // Parameter: decimal accountStatus
         public void SetAccountStatus(decimal accountStatus);
         public bool Cancel();
         
